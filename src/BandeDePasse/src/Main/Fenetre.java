30b4aa40965f23ce12452a6de117300fe14a3489
db200253 <db200253@DESKTOP-1K6SLDR.localdomain>
2023-01-14 11:56:38 +0100

    Correction et factorisation du code

--- src/BandeDePasse/src/Main/Fenetre.java
+++ src/BandeDePasse/src/Main/Fenetre.java
@@ -1,39 +1,90 @@
 package Main;
 
 import Fen.*;
+
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
 
+
 @SuppressWarnings("serial")
-public class Fenetre extends JFrame implements ActionListener{
-  
-  private JLabel titre1 = new JLabel("Entrez la trame binaire", SwingConstants.CENTER);
-  private JTextArea area = new JTextArea(1, 15);
-  private JLabel titre2 = new JLabel("Choisissez le code à utiliser :", SwingConstants.CENTER);
-  private JComboBox<String> code = new JComboBox<>();
-  private JButton bouton = new JButton("Draw !");
+public class Fenetre extends JFrame /*implements ActionListener*/{
   
   public Fenetre(){
     
-    setBounds(400, 150, 400, 150);
-    setTitle("Choix chaîne et code");
-    
-    JPanel panel = new JPanel();
-    panel.add(titre1);
-    panel.add(area);
-    panel.add(titre2);
-    code.addItem("NRZ");
-    code.addItem("Manchester");
-    code.addItem("Manchester diff.");
-    code.addItem("Miller");
-    panel.add(code);
-    panel.add(bouton);
-    bouton.addActionListener(this);
-    setContentPane(panel);
+    super("Bande de passe");
+    createComposants();
+    pack();
+    setLocationRelativeTo(null);
+    setDefaultCloseOperation(EXIT_ON_CLOSE);
+    setVisible(true);
+  }
+ 
+  public void createComposants() {
+	  
+	  JPanel principal = new JPanel();
+	  principal.setLayout(new BoxLayout(principal, BoxLayout.Y_AXIS));
+	  JPanel p1 = new JPanel();
+	  p1.setLayout(new BoxLayout(p1, BoxLayout.X_AXIS));
+	  JPanel p2 = new JPanel();
+	  p2.setLayout(new BoxLayout(p2, BoxLayout.X_AXIS));
+	  JPanel p3 = new JPanel();
+	  
+	  p1 = getP1(p1);
+	  p2 = getP2(p2);
+	  p3 = getP3(p3);
+	  	  
+	  principal.add(p1);
+	  principal.add(p2);
+	  principal.add(p3);
+	  //bouton.addActionListener(this);
+	  setContentPane(principal);
+  }
+  
+  public JPanel getP1(JPanel p) {
+	  
+	  JLabel titre1 = new JLabel("Entrez la trame binaire : ", SwingConstants.CENTER);
+	  JTextArea area = new JTextArea(1, 15);  
+	  
+	  p.add(Box.createRigidArea(new Dimension(10, 0)));
+	  p.add(titre1);
+	  p.add(Box.createHorizontalGlue());
+	  p.add(Box.createRigidArea(new Dimension(10, 0)));
+	  p.add(area);
+	  p.add(Box.createRigidArea(new Dimension(10, 0)));
+	  
+	  return p;
+  }
+  
+  public JPanel getP2(JPanel p) {
+	  
+	  JLabel titre2 = new JLabel("Choisissez le code à utiliser :", SwingConstants.CENTER);
+	  JComboBox<String> code = new JComboBox<>();
+	  code.addItem("NRZ");
+	  code.addItem("Manchester");
+	  code.addItem("Manchester diff.");
+	  code.addItem("Miller");
+	  
+	  p.add(Box.createRigidArea(new Dimension(10, 0)));
+	  p.add(titre2);
+	  p.add(Box.createHorizontalGlue());
+	  p.add(Box.createRigidArea(new Dimension(10, 0)));
+	  p.add(code);
+	  p.add(Box.createRigidArea(new Dimension(10, 0)));
+	  
+	  return p;
+  }
+  
+  public JPanel getP3(JPanel p) {
+	  
+	  JButton bouton = new JButton("Draw !");
+	  p.add(bouton);
+	  
+	  return p;
   }
 
-  @Override
+  /*@Override
   public void actionPerformed(ActionEvent e) {
     
     if(e.getSource() == bouton) {
@@ -56,5 +107,5 @@ public class Fenetre extends JFrame implements ActionListener{
         case "Miller" : fc = new FenMil(zone); fc.draw();
       }
     }
-  }
+  }*/
 }
